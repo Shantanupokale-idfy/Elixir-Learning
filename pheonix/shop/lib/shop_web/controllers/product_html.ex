e9d35d18270d794f4e771b027bc8190c48580d81
Shantanupokale-idfy <shantanu.pokale@idfy.com>
2026-01-26 23:48:12 +0530

    feat: created component and used it in heex file just like we create it in react and then use it

--- pheonix/shop/lib/shop_web/controllers/product_html.ex
+++ pheonix/shop/lib/shop_web/controllers/product_html.ex
@@ -1,5 +1,14 @@
 defmodule ShopWeb.ProductHTML do
   use ShopWeb, :html
-  
+
   embed_templates "product_html/*"
+
+
+
+  #definig component just like we use to define in react now we can reuse it anywhere
+  def product(assigns) do
+    ~H"""
+      <p> Product Component Game : <%= assigns.name %> </p>
+    """
+  end
 end
