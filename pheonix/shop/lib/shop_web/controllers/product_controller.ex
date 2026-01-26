2f0def40402802929955ec81a365ecc619607a50
Shantanupokale-idfy <shantanu.pokale@idfy.com>
2026-01-27 00:07:16 +0530

    refactor: applied for loop in keywordlist

--- pheonix/shop/lib/shop_web/controllers/product_controller.ex
+++ pheonix/shop/lib/shop_web/controllers/product_controller.ex
@@ -2,7 +2,7 @@ defmodule ShopWeb.ProductController do
   use ShopWeb , :controller
 
 
-@product [
+@products [
   %{id: "1" , name: "god of war"},
   %{id: "2" , name: "bayblade"},
   %{id: "3" , name: "gta5"}
@@ -10,12 +10,12 @@ defmodule ShopWeb.ProductController do
 
   def index(conn , _params) do
     conn
-    |> assign(:products , @product)
+    |> assign(:products , @products)
     |> render(:index )
   end
 
   def show(conn, %{"id" => id}) do
-    product = Enum.find(@product , fn product -> product.id == id end)
+    product = Enum.find(@products , fn product -> product.id == id end)
     conn
     |> assign(:product ,product)
     |> render( :show  )
